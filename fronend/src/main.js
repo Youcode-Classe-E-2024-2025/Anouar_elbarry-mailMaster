f2454236703da1928c3241905ccd252adf085440
anouar el barry <elbarryanwar37@gmail.com>
2025-04-11 12:25:36 +0100

    add SubscribersList

--- fronend/src/main.js
+++ fronend/src/main.js
@@ -2,9 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import dashboard from './views/dashboard.vue'
+
 
 
 const app = createApp(App)
+import dashboard from './views/dashboard.vue'
 app.component('dashboard',dashboard);
+
+import SubscribersList from './views/SubscribersList.vue'
+app.component('SubscribersList',SubscribersList);
+
 app.mount('#app')
